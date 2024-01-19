6d5ac7813c0ddccef21a8bbdd273099d49949099
Bris1127 <156917085+B204-hash@users.noreply.github.com>
2024-01-19 15:25:24 +0100

    Update UserController.java

--- src/main/java/InxhinieriSofti/projekt/Controllers/UserController.java
+++ src/main/java/InxhinieriSofti/projekt/Controllers/UserController.java
@@ -1,12 +1,1 @@
-package InxhinieriSofti.projekt.Repositories;
 
-import InxhinieriSofti.projekt.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("select u from User u where u.username = ?1")
-    User findByUsername(String username);
-
-    User findByEmail(String email);
-}
