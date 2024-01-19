9afa8c16b3c3d5da3f75bb5c882f12fb84494984
dasfhjlk <123780327+dasfhjlk@users.noreply.github.com>
2024-01-19 22:51:36 +0100

    Update UserController.java

--- src/main/java/InxhinieriSofti/projekt/Controllers/UserController.java
+++ src/main/java/InxhinieriSofti/projekt/Controllers/UserController.java
@@ -1,1 +1,44 @@
+package InxhinieriSofti.projekt.Controllers;
 
+import InxhinieriSofti.projekt.Models.User;
+import InxhinieriSofti.projekt.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(){
+    return "resources/templates/Registration";
+}
+
+    @PostMapping("/register")
+    public String registerUser(User user, Model model){
+        if(userRepository.findByUsername(user.getUsername()) != null){
+            model.addAttribute("error", "User already exists.");
+            return "register";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm(){
+        return "templates/Login";
+    }
+}
