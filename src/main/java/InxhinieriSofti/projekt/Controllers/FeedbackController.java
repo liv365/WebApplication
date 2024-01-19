11328de51ddfcd5c89ce1cd9fd481eba568264dd
dasfhjlk <123780327+dasfhjlk@users.noreply.github.com>
2024-01-19 22:47:14 +0100

    Update FeedbackController.java

--- src/main/java/InxhinieriSofti/projekt/Controllers/FeedbackController.java
+++ src/main/java/InxhinieriSofti/projekt/Controllers/FeedbackController.java
@@ -1,1 +1,67 @@
+package InxhinieriSofti.projekt.Controllers;
 
+import InxhinieriSofti.projekt.Models.Course;
+import InxhinieriSofti.projekt.Models.Feedback;
+import InxhinieriSofti.projekt.Services.FeedbackService;
+import InxhinieriSofti.projekt.Services.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class FeedbackController {
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+
+    @Autowired
+    private RegistrationService registrationService;
+
+
+    @GetMapping("/leave-feedback/{courseId}")
+    public String getLeaveFeedback(@PathVariable Long courseId, Model model, Principal principal) {
+        boolean isRegistered = registrationService.isUserRegistered(principal.getName(), courseId);
+
+
+        if (isRegistered) {
+            model.addAttribute("courseId", courseId);
+            return "LeaveFeedback";
+        } else {
+            return "redirect:/error";
+        }
+    }
+    @PostMapping("/leave-feedback")
+    public String leaveFeedback(@RequestParam Long courseId,
+                                @RequestParam String description,
+                                @RequestParam int rating,
+                                Principal principal) {
+        // Check if the student is registered for the course
+        boolean isRegistered = registrationService.isUserRegistered(principal.getName(), courseId);
+
+        if (isRegistered) {
+            feedbackService.leaveFeedback(courseId, principal.getName(), description, rating);
+            return "redirect:/courses/" + courseId;
+        } else {
+            // Redirect to an error page or handle accordingly
+            return "redirect:/error";
+        }
+    }
+
+
+    @GetMapping("/view-feedback/{courseId}")
+    public String viewFeedback(@PathVariable Long courseId, Model model) {
+        List<Feedback> feedbackList = feedbackService.getFeedbackForCourse(courseId);
+        double averageRating = feedbackService.calculateAverageRating(courseId);
+
+        model.addAttribute("feedbackList", feedbackList);
+        model.addAttribute("averageRating", averageRating);
+
+        return "FeedBackView";
+    }
+}
